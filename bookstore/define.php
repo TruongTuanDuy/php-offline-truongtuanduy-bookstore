6593f21985ba70b8ca513219345dad9cdb3f15a1
T480SD\TuanDUY <tuanduy.truong@gmail.com>
2023-05-17 23:15:05 +0700

    abc

--- bookstore/define.php
+++ bookstore/define.php
@@ -12,7 +12,7 @@ define('MODULE_PATH', APPLICATION_PATH . 'module' . DS);    // Định nghĩa đ
 define('BLOCK_PATH', APPLICATION_PATH . 'block' . DS);      // Định nghĩa đường dẫn đến thư mục block							
 define('TEMPLATE_PATH', PUBLIC_PATH . 'template' . DS);      // Định nghĩa đường dẫn đến thư mục template							
 
-define('ROOT_URL', DS . 'PHP-Class/bookstore' . DS);
+define('ROOT_URL', DS . 'PHP-Class/zvn-php-offline-truongtuanduy-bookstore/bookstore' . DS);
 define('APPLICATION_URL', ROOT_URL . 'application' . DS);
 define('PUBLIC_URL', ROOT_URL . 'public' . DS);
 define('UPLOAD_URL', PUBLIC_URL . 'files' . DS);
